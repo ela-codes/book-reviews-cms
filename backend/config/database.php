5d380a0d19c1d5bae42d8e84e01d5ed368d7eb76
Ela <teodocio.am@gmail.com>
2024-11-13 13:09:03 -0600

    Connect database using phpdotenv package

---
+++ backend/config/database.php
@@ -0,0 +1,17 @@
+<?php
+// Contains the the connection for mysql database using PDO.
+
+// Load environment variables using dotenv package
+require dirname(__DIR__, 2) . '/vendor/autoload.php'; // load composer
+Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__, 2))->safeLoad();
+
+try {
+    $db = new PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
+    print "Successful DB connection." . "\n";
+} catch (PDOException $e) {
+    print "Error: " . $e->getMessage();
+    die(); // Force execution to stop on errors.
+    // When deploying to production you should handle this
+    // situation more gracefully. ¯\_(ツ)_/¯
+}
+?>
