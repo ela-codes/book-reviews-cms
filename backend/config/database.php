4c9e525e5135d08863227fed00dd03b9f3e9e8eb
Ela <teodocio.am@gmail.com>
2024-11-13 16:23:27 -0600

    Add registration form and setup input sanitization and validation

--- backend/config/database.php
+++ backend/config/database.php
@@ -7,7 +7,6 @@ Dotenv\Dotenv::createUnsafeImmutable(dirname(__DIR__, 2))->safeLoad();
 
 try {
     $db = new PDO($_ENV["DB_DSN"], $_ENV["DB_USER"], $_ENV["DB_PASSWORD"]);
-    print "Successful DB connection." . "\n";
 } catch (PDOException $e) {
     print "Error: " . $e->getMessage();
     die(); // Force execution to stop on errors.
---
+++ frontend/register.php
@@ -0,0 +1,137 @@
+<?php
+require "../backend/config/database.php";
+require __DIR__ . '/../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// Create a logger instance
+$logger = new Logger('registration');
+$logger->pushHandler(new StreamHandler('./register.log', Logger::DEBUG));
+$logger->info('Registration page loaded');
+
+
+/**
+ * Checks whether the username and email exists in the user database.
+ * @param string $username A string representation of a username.
+ * @param string $username A string representation of an email.
+ * @return bool Whether the username and email exists in the user database. 
+ */
+function checkUserExists($db, $username, $email) {
+
+    $statement = $db->prepare("SELECT COUNT(*) FROM user WHERE username = :username AND email = :email");
+    $statement->bindValue(":username", strtolower(trim($username)));
+    $statement->bindValue(":email", strtolower(trim($email)));
+
+    $statement->execute();
+
+    $result = $statement->fetchColumn();
+
+    return $result > 0;
+}
+
+
+/**
+ * Checks whether the string of password matches.
+ */
+function checkPasswordMatch($password, $confirm_password) {
+    return strcmp(trim($password), trim($confirm_password)) == 0;
+}
+
+/**
+ * Checks whether the string of password passes the validation rule.
+ */
+function checkPasswordPattern($password) {
+    return preg_match("/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/", trim($password)) == 1;
+}
+
+/**
+ * Adds new user to database.
+ */
+function addUser($username, $password, $email) {
+
+}
+
+
+$passwordFeedback = "";
+
+
+/**
+ * Handle registration form's submitted data.
+ */
+if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm_password"])) {
+
+    // sanitize data
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $confirm_password = filter_input(INPUT_POST, 'confirm_password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+
+    $logger->debug("Form submitted: Username - $username, Email - $email, Password - $password, Confirm_Password - $confirm_password");
+
+    // perform data validation
+    if ($username && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+        $isExistingUser = checkUserExists($db, $username, $email);
+        $isMatchingPassword = checkPasswordMatch($password, $confirm_password);
+
+        if (!$isExistingUser && $isMatchingPassword) {
+            $isValidPasswordPattern = checkPasswordPattern($password);
+            if ($isValidPasswordPattern) {
+                addUser($username, $email, $password);
+            } else {
+                $passwordFeedback = "Passwords must be at least 6 characters long. \n It must contain 1 letter and 1 number.";
+            }
+        } else {
+            $passwordFeedback = "Both passwords must match.";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Reviews - Register</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container">
+        <h2 class="mt-5">Register</h2>
+        <form action="register.php" method="post" id="registerForm">
+            <ul class="list-unstyled">
+                <li>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" class="form-control" value="bobthebuilder" required/>
+                </li>
+                <li>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" value="bob@test.com" required/>
+                </li>
+                <li>
+                    <label for="password">Password</label>
+                    <input type="password" name="password" id="password" class="form-control" value="12345" required/>
+                </li>
+                <li>
+                    <label for="confirm_password">Confirm Password</label>
+                    <input type="password" name="confirm_password" id="confirm_password" class="form-control" value="12345" required/>
+                    <div id="passwordFeedback"><?= $passwordFeedback ?></div>
+                </li>
+                <button type="submit" class="btn btn-primary mt-3">Register</button>
+            </ul>
+        </form>
+    </div>
+
+    <!-- Link to Bootstrap JS and dependencies (jQuery and Popper.js) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
+
