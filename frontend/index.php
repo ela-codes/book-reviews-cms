f35dce496ddae31a6ae4fef8e7dbc92b3842d58e
Ela <teodocio.am@gmail.com>
2024-11-15 13:01:46 -0600

    Create barebones home page

--- frontend/index.php
+++ frontend/index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html class="h-100" lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Reviews - Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    
+<body class="d-flex h-100 text-center">
+    <div class="container-fluid d-flex flex-column">
+        <header class="mb-auto">
+            <nav class="navbar navbar-expand-sm">
+                <div class="container h-100">
+                    <a href="#" class="navbar-brand">BookReviews</a>
+                    <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#homeNav" aria-controls="homeNav" aria-label="Expand Navigation Bar">
+                        <div class="navbar-toggler-icon"></div>
+                    </button>
+                    <div class="collapse navbar-collapse" id="homeNav">
+                        <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Log In</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Browse</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <main class="row">
+            <div class="container w-50">
+                <h1>Welcome to BookReviews.</h1>
+                <h6>Discover, review, and share your love for books.</h6>
+                <a class="btn btn-dark btn-lg mt-4" href="./register.php">Register for FREE</a>
+
+            </div>
+        </main>
+        <footer class="mt-auto">
+            <p>Created by <a class="link-dark" href="https://github.com/ela-codes/book-reviews-cms">@ela-codes</a>.</p>
+        </footer>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
